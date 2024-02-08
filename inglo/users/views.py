19105ee64cf1a511a2857b2597f351e96d3a124e
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-09 01:01:03 +0900

    feat: signUp, Login api 구현

--- inglo/users/views.py
+++ inglo/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from rest_framework import status, views
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class SignUpView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Successfully logged in."}, status=status.HTTP_200_OK)
+        return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
