1e8ed135bcbfedf63e4307cfdd2cda3c68f5209c
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-10 17:57:00 +0900

    fix: modify 'DEFAULT_AUTHENTICATION_CLASSES' to JWT

--- inglo/inglo/settings.py
+++ inglo/inglo/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
