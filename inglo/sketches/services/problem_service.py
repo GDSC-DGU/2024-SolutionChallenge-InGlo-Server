185a1f50b856e3b8d38b86c15b4853dea25382e3
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-12 00:29:12 +0900

    fix: modify error handler

--- inglo/sketches/services/problem_service.py
+++ inglo/sketches/services/problem_service.py
@@ -3,10 +3,9 @@ from ..models import Problem
 class ProblemService:
     @staticmethod
     def get_problems_by_sdgs(sdgs):
-        if not sdgs.isdigit() or not 1 <= int(sdgs) <= 17:
+        if not 1 <= int(sdgs) <= 17:
             return Problem.objects.none()
         try:
-            sdgs = int(sdgs)
             return Problem.objects.filter(sdgs=sdgs).order_by('-created_at')[:10]
         except Problem.DoesNotExist:
             return Problem.objects.none()
@@ -14,7 +13,6 @@ class ProblemService:
     @staticmethod
     def create_problem(sdgs, content):
         try:
-            sdgs = int(sdgs)
             problem = Problem.objects.create(sdgs=sdgs, content=content)
             return problem
         except (ValueError, TypeError):
