f43d04da6c159107794d4b674e1cfbd7f1866583
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-15 06:20:58 +0900

    fix: Post 상세보기에서 피드백이 source지정으로 조회되지 않던 문제 수정

--- inglo/posts/serializers.py
+++ inglo/posts/serializers.py
@@ -5,7 +5,7 @@ from sketches.serializers import SketchNestedSerializer
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id', 'user', 'title', 'sdgs', 'likes', 'created_at']
 
 class FeedbackSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()  # 사용자 이름 표시
@@ -20,15 +20,16 @@ class PostLikeSerializer(serializers.ModelSerializer):
 
 class PostDetailSerializer(serializers.ModelSerializer):
     sketch = SketchNestedSerializer(read_only=True)  # 인스턴스화
-    feedbacks = FeedbackSerializer(many=True, read_only=True, source='feedbacks')
+    feedbacks = FeedbackSerializer(many=True, read_only=True)
     is_liked = serializers.SerializerMethodField()  # 현재 사용자가 좋아요를 눌렀는지 여부
+
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'sketch', 'title', 'content', 'sdgs', 'likes', 'created_at', 'feedbacks', 'is_liked']
 
     def get_is_liked(self, obj):
         user = self.context['request'].user
         return PostLike.objects.filter(post=obj, user=user).exists()
 
-    class Meta:
-        model = Post
-        fields = '__all__'  # 'is_liked'는 fields에 이미 포함됩니다.
     
     
