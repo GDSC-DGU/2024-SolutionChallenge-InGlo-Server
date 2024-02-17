5fa99dc7b3e3b01a8eef5939bd516559e18ac229
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-18 06:52:20 +0900

    feat: 스케치 img 파일을 s3에 업로드한 후 해당 url을 DB에 저장하는 로직 추가

--- inglo/sketches/services/sketch_service.py
+++ inglo/sketches/services/sketch_service.py
@@ -1,13 +1,35 @@
 from ..models import Sketch
 from ..models import Problem
+from dotenv import load_dotenv
+import requests
+from io import BytesIO
+import os
+import boto3
+import magic
+from urllib.parse import urlparse
 
 class SketchService:
     def get_sketches_by_user(user):
         return Sketch.objects.filter(user=user)
     
-    def update_sketch(user,problem_id,title,description,image_url,content):
+    def update_sketch(user,problem_id,title,description,image,content):
         problem = Problem.objects.get(id = problem_id)
         sketch = Sketch.objects.filter(user=user, problem=problem).order_by('-created_at').first()
+
+        s3_resource = boto3.resource('s3',
+                                     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                                     region_name=os.getenv('AWS_REGION_NAME'))
+        bucket_name = os.getenv('AWS_STORAGE_BUCKET_NAME')
+        file_path = f'user_{user.id}/sketch_{sketch.id}'  # S3 내에서 파일을 저장할 경로
+
+        mime_type = magic.from_buffer(image.read(2048), mime=True)
+        image.seek(0)  
+
+        s3_resource.Bucket(bucket_name).put_object(Key=file_path, Body=image, ContentType=mime_type)
+
+        image_url = f"https://{bucket_name}.s3.{os.getenv('AWS_REGION_NAME')}.amazonaws.com/{file_path}"
+
         sketch.title = title
         sketch.description = description
         sketch.image_url = image_url
