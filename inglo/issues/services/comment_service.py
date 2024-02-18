ade565ecfd7648fb3aac999e7f179e4757ddb08c
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-18 22:31:48 +0900

    fix: IssueCommentUpdateDeleteViewSet 업데이트 메소드 명 수정 및 예외처리

--- inglo/issues/services/comment_service.py
+++ inglo/issues/services/comment_service.py
@@ -4,15 +4,26 @@ from django.core.exceptions import PermissionDenied
 from django.db import transaction
 
 class CommentService:
+
+    @staticmethod
+    def get_all_comments():
+        return IssueComment.objects.all()
+        
+
     @staticmethod
     @transaction.atomic
     def create_comment(user, issue_id, data):
         issue = get_object_or_404(Issue, pk=issue_id)
+        parent_comment_id = data['parent_comment']
+        if parent_comment_id and parent_comment_id.isdigit():
+            parent_comment_id = int(parent_comment_id)
+        else:
+            parent_comment_id = None
         comment = IssueComment.objects.create(
             user=user,
             issue=issue,
             content=data['content'],
-            parent_comment_id=int(data['parent_comment']) if 'parent_comment' in data else None
+            parent_comment_id=parent_comment_id
         )
         return comment
 
