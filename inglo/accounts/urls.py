6e4f7b37e3944fa2751cb7a70a0813f449816781
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-20 00:13:24 +0900

    fix: api 테스트용 로그인 로직으로 변경

--- inglo/accounts/urls.py
+++ inglo/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.conf.urls import include
 from accounts.views import (
-    # GoogleLoginView,
-    CustomGoogleLoginView,
+    GoogleLoginView,
+    # CustomGoogleLoginView,
     AdditionalUserInfoView,
     ProfileImageUploadView,
     UserDetailViewSet
@@ -15,9 +15,9 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
     path('', include('allauth.urls')),
-    # path('google/login/', GoogleLoginView.as_view(), name='api_accounts_google_oauth'), # api 테스트용
-    path('login-success/', CustomGoogleLoginView.as_view(), name='custom_google_login'), # 배포 환경 용
-    # path('login-success/', GoogleLoginView.get_response, name='login-success'),
+    path('google/login/', GoogleLoginView.as_view(), name='api_accounts_google_oauth'), # api 테스트용
+    # path('login-success/', CustomGoogleLoginView.as_view(), name='custom_google_login'), # 배포 환경 용
+    path('login-success/', GoogleLoginView.get_response, name='login-success'),
     path('additional_info/', AdditionalUserInfoView.as_view(), name='additional_info'),
     path('info/', UserDetailViewSet.as_view({'get':'retrieve','patch':'update'}), name='user-info'),
     path('info/profile-img/', ProfileImageUploadView.as_view(), name='profile_image_upload'),
