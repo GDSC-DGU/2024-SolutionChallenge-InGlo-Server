cd82e5510b7fc0e26299674e930f9ba96bca0e89
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-20 23:30:28 +0900

    fix: 분류 모델 반환값 변경. (국가,sdgs) -> (sdgs)

--- inglo/issues/utils/classifier.py
+++ inglo/issues/utils/classifier.py
@@ -1,11 +1,10 @@
 # (제목,내용)을 바탕으로 (국가,SDGs)를 분류하는 분류모델
 
-def classify_news(title, content):
+def classify_news(content):
     """
-    뉴스 기사의 제목과 내용을 바탕으로
-    국가와 SDGs를 분류하는 모델
+    뉴스 기사의 내용을 바탕으로
+    SDGs를 분류하는 모델
     """
-    # 분류 모델이 (제목, 내용)을 가지고 '어떤 국가와 관련된 기사'인지, '어떤 SDGs와 관련된 기사'인지 분류한다고 가정.
-    country = '1'  # 국가는 1번으로 가정
+    # 분류 모델이 내용을 가지고 '어떤 SDGs와 관련된 기사'인지 분류한다고 가정.
     sdgs = '1'  # SDGs는 1번으로 가정
-    return country, sdgs
+    return sdgs
