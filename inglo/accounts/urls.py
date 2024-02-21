e7c4b4768098d1e5f81c14dced4740cd2f940449
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-21 22:26:54 +0900

    fix: 서비스 로직 분리

--- inglo/accounts/urls.py
+++ inglo/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf.urls import include
 from accounts.views import (
-    # GoogleLoginView,
     CustomGoogleLoginView,
     ProfileImageUploadView,
     UserDetailViewSet,
@@ -16,10 +15,8 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('dj_rest_auth.registration.urls')),
     path('', include('allauth.urls')),
-    # path('google/login/', GoogleLoginView.as_view(), name='api_accounts_google_oauth'), # api 테스트용
     path('loginsuccess/', CustomGoogleLoginView.as_view(), name='custom_google_login'), # 배포 환경 용
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    # path('login-success/', GoogleLoginView.get_response, name='login-success'),
     path('info/', UserDetailViewSet.as_view({'get':'retrieve','patch':'update'}), name='user-info'),
     path('info/profile-img/', ProfileImageUploadView.as_view(), name='profile_image_upload'),
     path('additional-info/', AdditionalInfoProvidedView.as_view(), name='additional-info'),
