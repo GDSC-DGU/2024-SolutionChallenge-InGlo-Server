10b39bc6f6517ea1bf7f28285e50449dbf91ba10
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-23 02:06:38 +0900

    fix: database utf 인코딩 설정 추가

--- inglo/inglo/settings.py
+++ inglo/inglo/settings.py
@@ -118,6 +118,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
