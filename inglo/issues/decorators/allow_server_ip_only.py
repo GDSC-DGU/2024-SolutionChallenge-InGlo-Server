85a65a011774044dd6114704b7100f99dd16db8f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-02-24 23:47:38 +0900

    fix: 외부  ip에서도 이슈 생성 api가 호출되는 버그 발견. 임시 조치

--- inglo/issues/decorators/allow_server_ip_only.py
+++ inglo/issues/decorators/allow_server_ip_only.py
@@ -10,8 +10,8 @@ def allow_server_ip_only(view_func):
         logger.info('client IP: ' + request.META['REMOTE_ADDR'])
         my_ip = os.getenv('MYIP')
         my_ip2 = os.getenv('MYIP2')
-        logger.info('my IP: ' + my_ip)
-        allowed_ips = ['127.0.0.1', my_ip, my_ip2 ]  # 허용할 IP 주소
+        my_ip3 = os.getenv('MYIP3')
+        allowed_ips = [my_ip, my_ip2, my_ip3]  # 허용할 IP 주소
         if request.META['REMOTE_ADDR'] not in allowed_ips:
             return HttpResponseForbidden("Access denied.")
         return view_func(request, *args, **kwargs)
